773798f0e7dafe0ed6fe94e96311858869234794
Наташа <tertyshnayanatasha@gmail.com>
2023-02-20 00:40:54 +0100

    fixed

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,33 +1,35 @@
 const navToggle = document.querySelector('.header_toggler');
-console.log(navToggle);
 const overlay = document.querySelector('.nav');
-console.log(overlay);
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
     overlay.classList.toggle("nav_overlay");
 })
 
-// Выбираем кнопку
+// Change theme
+
 const btn = document.querySelector('.button-theme');
-// Отслеживаем щелчок по кнопке
-btn.addEventListener('click', function() {
-  // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-  document.body.classList.toggle('dark-theme'); 
+
+btn.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+
+    let bodyTheme = document.body.className;
+    localStorage.setItem('theme', bodyTheme);
 })
 
+document.body.className = localStorage.getItem('theme');
 
 //Accordion
 let label = document.querySelectorAll(".accordion_item")
 
-label.forEach((e)=>{
-    e.addEventListener("click", ()=>{
+label.forEach((e) => {
+    e.addEventListener("click", () => {
         removeClass()
         e.classList.toggle("active")
     })
 })
 
-function removeClass(){
-    label.forEach((e)=>{
+function removeClass() {
+    label.forEach((e) => {
         e.classList.remove("active")
     })
 }
